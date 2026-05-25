63a23535c5450541d2a20512db83b286b8773550
aguirrealexis237 <aguirrealexis237@gmail.com>
2026-05-25 21:37:45 +0000

    FIX: Versiones corregidas

--- Punto_E.sh
+++ Punto_E.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-mkdir -p ~/Punto_E
-free -h | grep "Mem:" | awk '{print "Total de memoria RAM: " $2}' >Punto_E/Filtro_basico.txt
-lscpu | grep "Model name:" | awk -F: '{print "Modelo y fecuencia: " $2} | sed 's/^[ \t]*//' >> ~/Punto_E/Filtro_basico.txt
+free -h | grep "Mem:" | awk '{print "Total de memoria RAM: " $2}' > ~/Punto_E/Filtro_basico.txt
+lscpu | grep "Model name:" | awk -F: '{print "Modelo y frecuencia: " $2}' | sed 's/^[ \t]*//' >> ~/Punto_E/Filtro_basico.txt
